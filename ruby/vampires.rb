d393a5732dd6eaeb94471e01278a35a07324b4f2
Kenn Sugiyama <kenn@KBSugiyama.com>
2017-02-14 21:32:53 -0800

    release3: add multiple employee function

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,51 +1,63 @@
-puts "What is your name?"
-name = gets.chomp.downcase
+$result = nil
 
-puts "How old are you?"
-age = gets.chomp.to_i
-
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y or N)"
-garlic_response=gets.chomp.downcase
-like_garlic = garlic_response == "y"
-
-puts "Would you like to enroll in the company's health insurance? (Y or N)"
-want_insurance = gets.chomp == "y"
+puts "How many employees would you like to verify?"
+i = no_employees = gets.chomp.to_i
 
 
+def werewolf_verification()
+	puts "What is your name?"
+	name = gets.chomp.downcase
 
-def age_check_Pass(age, b_year) #verify age
-	(Time.now.year - b_year) == age
-end 
+	puts "How old are you?"
+	age = gets.chomp.to_i
+
+	puts "What year were you born?"
+	birth_year = gets.chomp.to_i
+
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y or N)"
+	garlic_response=gets.chomp.downcase
+	like_garlic = garlic_response == "y"
+
+	puts "Would you like to enroll in the company's health insurance? (Y or N)"
+	want_insurance = gets.chomp == "y"
 
 
-def name_check_Pass (name)
-	!(name=="drake cula" || name=="tu fang")
+
+	def age_check_Pass(age, b_year) #verify age
+		(Time.now.year - b_year) == age
+	end 
+
+
+	def name_check_Pass (name)
+		!(name=="drake cula" || name=="tu fang")
+	end
+
+
+
+
+	if age_check_Pass(age, birth_year) && (like_garlic || want_insurance)
+		$result = "Probably not a vampire."
+	end
+
+	if !age_check_Pass(age, birth_year) && (!like_garlic || !want_insurance)
+		$result = "Probably a vampire."
+	end
+
+	if !age_check_Pass(age, birth_year) && !like_garlic && !want_insurance
+		$result = "Almost certainly a vampire"
+	end
+
+	if !name_check_Pass(name)
+		$result = "Clearly a vampire"
+	end
+
+	if $result == nil
+		$result = "Results are inconclusive."
+	end
 end
 
-
-
-
-if age_check_Pass(age, birth_year) && (like_garlic || want_insurance)
-	result = "Probably not a vampire."
+while i > 0
+	werewolf_verification
+	p $result
+	i = i-1
 end
-
-if !age_check_Pass(age, birth_year) && (!like_garlic || !want_insurance)
-	result = "Probably a vampire."
-end
-
-if !age_check_Pass(age, birth_year) && !like_garlic && !want_insurance
-	result = "Almost certainly a vampire"
-end
-
-if !name_check_Pass(name)
-	result = "Clearly a vampire"
-end
-
-if result == nil
-	result = "Results are inconclusive."
-end
-
-p result
