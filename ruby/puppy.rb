380809b20162944b27eb4cabed8256cdb28ec888
Kenn Sugiyama <kenn@KBSugiyama.com>
2017-02-28 13:26:04 -0800

    Complete Mod6-1

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+=begin
+Class: Puppies
+
+Attributes (characteristics)
+1 Size: Caries - Small, Medium, Large
+2 Energy-level: 0 - 10
+3 Noise-level: 0 - 10
+4 Name: Varies
+5 Age: Varies
+
+Methods (behavior)
+1 Bark
+2 Jump
+3 Run
+4 Play
+5 Eat
+6 Sleep
+7 Rough house
+
+=end
+
